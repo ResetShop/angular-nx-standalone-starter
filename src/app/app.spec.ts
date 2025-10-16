b71b8c60b38de888b30ee5376941de2f015cabb5
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-16 17:54:34 -0300

    [#4] - Update App component and its tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,22 @@
-import { render, screen } from '@testing-library/angular';
-import { App } from './app';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { Analytics } from './providers/analytics/analytics';
-import { AnalyticsMock } from './providers/analytics/analytics.mock';
+import { render } from "@testing-library/angular";
+import { App } from "./app";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { Analytics } from "@providers/analytics/analytics";
+import { AnalyticsMock } from "@providers/analytics/analytics.mock";
 
-describe('App', () => {
-  it('should render title', async () => {
-    await render(App, {
+describe("App", () => {
+  it("should create the app component", async () => {
+    const { fixture } = await render(App, {
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        provideRouter([]),
         { provide: Analytics, useClass: AnalyticsMock },
       ],
     });
 
-    const title = screen.getByText(
-      'Welcome to the Reset Dev Nx + Angular SSR starter repo 👋'
-    );
-    expect(title).toBeInTheDocument();
+    expect(fixture.componentInstance).toBeTruthy();
   });
 });
