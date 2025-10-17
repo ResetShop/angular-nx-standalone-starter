e9886a7614c1cc6fd0a576cecd3403f809e85ac7
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-17 10:41:49 -0300

    [#4] - Code styling changes

--- src/app/providers/analytics/analytics.ts
+++ src/app/providers/analytics/analytics.ts
@@ -4,28 +4,28 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class Analytics {
-  async init() {
-    // TODO: Uncomment if you're using Vercel Speed Insights
-    try {
-      // injectSpeedInsights();
-    } catch (error) {
-      console.error('Failed to initialize Speed Insights:', error);
-    }
+	async init() {
+		// TODO: Uncomment if you're using Vercel Speed Insights
+		try {
+			// injectSpeedInsights();
+		} catch (error) {
+			console.error('Failed to initialize Speed Insights:', error);
+		}
 
-    if (!environment.clarityProjectId) {
-      return;
-    }
+		if (!environment.clarityProjectId) {
+			return;
+		}
 
-    // TODO: Uncomment if you're using Clarity analytics, after installing the @microsoft/clarity package, setting the environment variable CLARITY_PROJECT_ID and setting up the api/helpers/environment.ts file
-    try {
-      // const clarityModule = await import('@microsoft/clarity');
-      // const clarity = clarityModule.default;
-      // clarity.init(environment.clarityProjectId);
-    } catch (error) {
-      console.error('Failed to initialize Clarity:', error);
-    }
-  }
+		// TODO: Uncomment if you're using Clarity analytics, after installing the @microsoft/clarity package, setting the environment variable CLARITY_PROJECT_ID and setting up the api/helpers/environment.ts file
+		try {
+			// const clarityModule = await import('@microsoft/clarity');
+			// const clarity = clarityModule.default;
+			// clarity.init(environment.clarityProjectId);
+		} catch (error) {
+			console.error('Failed to initialize Clarity:', error);
+		}
+	}
 }
