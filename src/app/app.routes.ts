4f4c8e78c5a193ef3d6e0865dac97b74534b5723
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-17 10:40:30 -0300

    [#4] - Code styling changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,32 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 
-export const validRoutes = [
-  "",
-  "health",
-  "auth",
-  "auth/login",
-  "auth/reset-password",
-];
+export const validRoutes = ['', 'health', 'auth', 'auth/login', 'auth/reset-password'];
 
 export const appRoutes: Route[] = [
-  {
-    path: "welcome",
-    loadComponent: () => import("./pages/welcome/welcome"),
-  },
-  {
-    path: "health",
-    loadComponent: () => import("./pages/health/health"),
-  },
-  {
-    path: "auth",
-    children: [
-      {
-        path: "login",
-        loadComponent: () => import("./pages/login/login"),
-      },
-      {
-        path: "reset-password",
-        loadComponent: () => import("./pages/reset-password/reset-password"),
-      },
-    ],
-  },
-  {
-    path: "**",
-    redirectTo: "auth/login",
-    pathMatch: "full",
-  },
+	{
+		path: 'welcome',
+		loadComponent: () => import('./pages/welcome/welcome'),
+	},
+	{
+		path: 'health',
+		loadComponent: () => import('./pages/health/health'),
+	},
+	{
+		path: 'auth',
+		children: [
+			{
+				path: 'login',
+				loadComponent: () => import('./pages/login/login'),
+			},
+			{
+				path: 'reset-password',
+				loadComponent: () => import('./pages/reset-password/reset-password'),
+			},
+		],
+	},
+	{
+		path: '**',
+		redirectTo: 'auth/login',
+		pathMatch: 'full',
+	},
 ];
--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,22 +1,22 @@
-import { render } from "@testing-library/angular";
-import { App } from "./app";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
-import { provideHttpClient } from "@angular/common/http";
-import { provideRouter } from "@angular/router";
-import { Analytics } from "@providers/analytics/analytics";
-import { AnalyticsMock } from "@providers/analytics/analytics.mock";
+import { render } from '@testing-library/angular';
+import { App } from './app';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { Analytics } from '@providers/analytics/analytics';
+import { AnalyticsMock } from '@providers/analytics/analytics.mock';
 
-describe("App", () => {
-  it("should create the app component", async () => {
-    const { fixture } = await render(App, {
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting(),
-        provideRouter([]),
-        { provide: Analytics, useClass: AnalyticsMock },
-      ],
-    });
+describe('App', () => {
+	it('should create the app component', async () => {
+		const { fixture } = await render(App, {
+			providers: [
+				provideHttpClient(),
+				provideHttpClientTesting(),
+				provideRouter([]),
+				{ provide: Analytics, useClass: AnalyticsMock },
+			],
+		});
 
-    expect(fixture.componentInstance).toBeTruthy();
-  });
+		expect(fixture.componentInstance).toBeTruthy();
+	});
 });
