f2583bd6922b8f68f7953780e73af6f0328f95ba
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-17 10:41:13 -0300

    [#4] - Code styling changes

--- jest.config.ts
+++ jest.config.ts
@@ -1,33 +1,30 @@
 export default {
-  displayName: "app",
-  testEnvironment: "@happy-dom/jest-environment",
-  preset: "./jest.preset.js",
-  setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
-  coverageDirectory: "./coverage/app",
-  transform: {
-    "^.+\\.(ts|mjs|js|html)$": [
-      "jest-preset-angular",
-      {
-        tsconfig: "<rootDir>/tsconfig.spec.json",
-        stringifyContentPathRegex: "\\.(html|svg)$",
-      },
-    ],
-  },
-  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
-  snapshotSerializers: [
-    "jest-preset-angular/build/serializers/no-ng-attributes",
-    "jest-preset-angular/build/serializers/ng-snapshot",
-    "jest-preset-angular/build/serializers/html-comment",
-  ],
-  testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
-    "<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)",
-  ],
-  moduleNameMapper: {
-    "^@models/(.*)$": "<rootDir>/src/app/models/$1",
-    "^@utils/(.*)$": "<rootDir>/src/app/utils/$1",
-    "^@mocks/(.*)$": "<rootDir>/src/app/mocks/$1",
-    "^@components/(.*)$": "<rootDir>/src/app/components/$1",
-    "^@providers/(.*)$": "<rootDir>/src/app/providers/$1",
-  },
+	displayName: 'app',
+	testEnvironment: '@happy-dom/jest-environment',
+	preset: './jest.preset.js',
+	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+	coverageDirectory: './coverage/app',
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': [
+			'jest-preset-angular',
+			{
+				tsconfig: '<rootDir>/tsconfig.spec.json',
+				stringifyContentPathRegex: '\\.(html|svg)$',
+			},
+		],
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	snapshotSerializers: [
+		'jest-preset-angular/build/serializers/no-ng-attributes',
+		'jest-preset-angular/build/serializers/ng-snapshot',
+		'jest-preset-angular/build/serializers/html-comment',
+	],
+	testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)', '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)'],
+	moduleNameMapper: {
+		'^@models/(.*)$': '<rootDir>/src/app/models/$1',
+		'^@utils/(.*)$': '<rootDir>/src/app/utils/$1',
+		'^@mocks/(.*)$': '<rootDir>/src/app/mocks/$1',
+		'^@components/(.*)$': '<rootDir>/src/app/components/$1',
+		'^@providers/(.*)$': '<rootDir>/src/app/providers/$1',
+	},
 };
