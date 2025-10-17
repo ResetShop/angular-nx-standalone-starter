fcb13d755f7d1e2d0e99c6a3b44a161b2c99fe7b
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-17 10:39:54 -0300

    [#4] - Code styling changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,24 @@
 export default {
-  stories: [
-    '../src/app/**/*.stories.mdx',
-    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  styles: [],
-  addons: [
-    '@storybook/addon-docs',
-    {
-      name: '@storybook/addon-styling-webpack',
-      options: {
-        postCss: true,
-      },
-    },
-  ],
-  framework: {
-    name: '@storybook/angular',
-    options: {
-      enableIvy: true,
-      enableProdMode: false,
-    },
-  },
-  docs: {},
-  staticDirs: [], //TODO: Add your project assets directory here, if needed
+	stories: ['../src/app/**/*.stories.mdx', '../src/app/**/*.stories.@(js|jsx|ts|tsx)'],
+	styles: [],
+	addons: [
+		'@storybook/addon-docs',
+		{
+			name: '@storybook/addon-styling-webpack',
+			options: {
+				postCss: true,
+			},
+		},
+	],
+	framework: {
+		name: '@storybook/angular',
+		options: {
+			enableIvy: true,
+			enableProdMode: false,
+		},
+	},
+	docs: {},
+	staticDirs: [], //TODO: Add your project assets directory here, if needed
 };
 
 // To customize your webpack configuration you can use the webpackFinal field.
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,8 @@
 import healthController from './modules/health/health.controller';
 
 export default [
-  {
-    path: '/health',
-    controller: healthController,
-  },
+	{
+		path: '/health',
+		controller: healthController,
+	},
 ];
