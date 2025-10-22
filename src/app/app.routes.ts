d17ac3944b0deebec6a1c60bb626f019c3469aab
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-22 01:28:29 -0300

    [#27] - Declare title names for routes using NamedRoute interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Route } from '@angular/router';
+import { NamedRoute } from '@interfaces/navigation';
 
 export const appRoutes: Route[] = [
 	{
 		path: 'auth',
+		title: 'Autenticación',
 		loadChildren: () => import('./pages/auth.routes'),
 	},
 	{
 		path: 'dashboard',
+		title: 'Panel Principal',
 		loadChildren: () => import('./pages/dashboard/dashboard.routes'),
 	},
 	{
 		path: '**',
+		title: 'Wildcard',
 		redirectTo: 'auth/login',
 		pathMatch: 'full',
 	},
-];
+] satisfies NamedRoute[];
