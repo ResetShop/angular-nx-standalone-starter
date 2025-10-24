5c3763316b48b592570ffeeb2fa221ab57c59494
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-24 12:55:08 -0300

    [#27] - Adds Brand component

---
+++ src/app/components/brand/brand.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Button } from '@components/button/button';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { RouterLink } from '@angular/router';
+import { featherRefreshCw } from '@ng-icons/feather-icons';
+
+@Component({
+	selector: 'app-brand',
+	imports: [Button, NgIcon, RouterLink],
+	template: `
+		<!--	TODO: Replace with the navigation to your home page / initial page -->
+		<a [routerLink]="['welcome']" [fullWidth]="true" appButton variant="default" size="sm" class="gap-2 font-semibold">
+			<ng-icon name="featherRefreshCw" />
+			<span>Reset Starter Repo</span>
+		</a>
+	`,
+	styles: `
+		:host {
+			@reference "tailwindcss";
+			@apply flex items-center p-2;
+		}
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	viewProviders: [provideIcons({ featherRefreshCw })],
+})
+export class Brand {}
