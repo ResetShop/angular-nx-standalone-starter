94b10108787a96e128772fac2d41feaf38dcbbb8
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-10-31 09:41:34 -0300

    [#20] - Replaces authResource for regular rxjs-based http request

--- src/app/providers/auth/auth.ts
+++ src/app/providers/auth/auth.ts
@@ -1,35 +1,27 @@
 import { isPlatformServer } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
 import { LoginFormParams, LoginResponse } from '@interfaces/auth';
-import { of, tap } from 'rxjs';
+import { tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class Auth {
 	private http = inject(HttpClient);
 	private platformId = inject(PLATFORM_ID);
 
-	readonly currentUser = computed(() => this._currentUser() ?? this.authResource.value());
+	readonly currentUser = computed(() => this._currentUser());
 	readonly isAuthenticated = computed(() => !!this._currentUser());
 
-	readonly loginRequestParams = signal<LoginFormParams | null>(null);
 	private readonly _currentUser = signal<null | LoginResponse>(null);
 
-	private authResource = rxResource({
-		params: this.loginRequestParams,
-		stream: ({ params }) => {
-			if (!params) {
-				return of(null);
-			}
-			return this.http.post<LoginResponse>('/api/auth/login', params).pipe(
-				tap((response) => {
-					localStorage.setItem('auth_user', JSON.stringify(response.user));
-					this._currentUser.set(response);
-				}),
-			);
-		},
-	});
+	login(params: LoginFormParams) {
+		return this.http.post<LoginResponse>('/api/auth/login', params).pipe(
+			tap((response) => {
+				localStorage.setItem('auth_user', JSON.stringify(response.user));
+				this._currentUser.set(response);
+			}),
+		);
+	}
 
 	retrieveFromStorage() {
 		if (isPlatformServer(this.platformId)) {
