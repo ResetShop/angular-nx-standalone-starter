6c2896cf4f210f39027c0010e7fd3f9cd92a81e1
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-11-13 11:17:19 -0300

    [#20] - Adds handling of loading state for Auth service

--- src/app/providers/auth/auth.ts
+++ src/app/providers/auth/auth.ts
@@ -11,6 +11,13 @@ export class Auth {
 
 	readonly currentUser = computed(() => this._currentUser());
 	readonly isAuthenticated = computed(() => !!this._currentUser());
+	readonly isInitialized = signal(false);
+	readonly isGuardValidated = signal(false);
+	private readonly minLoadingTimeElapsed = signal(false);
+
+	readonly isLoadingComplete = computed(
+		() => this.isInitialized() && this.isGuardValidated() && this.minLoadingTimeElapsed(),
+	);
 
 	private readonly _currentUser = signal<null | LoginResponse>(null);
 
@@ -25,12 +32,19 @@ export class Auth {
 
 	retrieveFromStorage() {
 		if (isPlatformServer(this.platformId)) {
+			this.isInitialized.set(true);
 			return;
 		}
 		const storedUser = localStorage.getItem('auth_user');
 		if (storedUser) {
 			this._currentUser.set(JSON.parse(storedUser));
 		}
+		this.isInitialized.set(true);
+
+		// Ensure loading screen is visible for a minimum amount of time
+		setTimeout(() => {
+			this.minLoadingTimeElapsed.set(true);
+		}, 1000);
 	}
 
 	logout() {
