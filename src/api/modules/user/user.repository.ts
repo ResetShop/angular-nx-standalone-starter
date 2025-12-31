91c2cb4069b2e0bdef7d32ea1418df871f69b2e1
Ramiro Olivencia <ramiro@olivencia.com.ar>
2025-12-31 14:42:11 -0300

    [#46] - Adds findById function to UserRepository

--- src/api/modules/user/user.repository.ts
+++ src/api/modules/user/user.repository.ts
@@ -11,6 +11,10 @@ export interface UserData {
 }
 
 export class UserRepository extends BaseRepository {
+	/**
+	 * Finds a user by their email address
+	 * @param email Email address to search for
+	 */
 	async findByEmail(email: string): Promise<UserData | null> {
 		const result = await this.db
 			.select({
@@ -22,6 +26,26 @@ export class UserRepository extends BaseRepository {
 			})
 			.from(user)
 			.where(eq(user.email, email))
+			.limit(1);
+
+		return result.length > 0 ? result[0] : null;
+	}
+
+	/**
+	 * Finds a user by their provided userId
+	 * @param id Email address to search for
+	 */
+	async findById(id: number): Promise<UserData | null> {
+		const result = await this.db
+			.select({
+				id: user.id,
+				email: user.email,
+				firstName: user.firstName,
+				lastName: user.lastName,
+				enabled: user.enabled,
+			})
+			.from(user)
+			.where(eq(user.id, id))
 			.limit(1);
 
 		return result.length > 0 ? result[0] : null;
