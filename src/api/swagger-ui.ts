6072f17d3ad8269c6bd09648c7bb7d721f7dfbc1
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-02 17:33:50 -0300

    Add SRI hashes to Swagger UI CDN assets and lighter commonAuthResponses variant

--- src/api/swagger-ui.ts
+++ src/api/swagger-ui.ts
@@ -7,11 +7,11 @@ export function buildSwaggerHtml(): string {
 <head>
 	<meta charset="UTF-8" />
 	<title>API Documentation</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.32.0/swagger-ui.css" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.32.0/swagger-ui.css" integrity="sha384-3nuX7df3EaAoiqLBeyS1Ola0Gpg9ryJKVtarubwfnA1cOH8AWHUdbPSIvEqPZ9VH" crossorigin="anonymous" />
 </head>
 <body>
 	<div id="swagger-ui"></div>
-	<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.32.0/swagger-ui-bundle.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5.32.0/swagger-ui-bundle.js" integrity="sha384-7xcoc6ZKDFF7Ek627QTC3Bg/K+5Y36NJ8MWAE43D2m6+3Sh9XO3tdsfHhrS8gNIQ" crossorigin="anonymous"></script>
 	<script>
 		SwaggerUIBundle({ url: '${specUrl}', dom_id: '#swagger-ui' });
 	</script>
