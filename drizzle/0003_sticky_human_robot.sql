3e1cc529656493681bee302f307776fba7034241
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-03 10:27:56 -0300

    [#120] Consolidate all migrations into a single initial migration

--- drizzle/0003_sticky_human_robot.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "role" ADD COLUMN "code" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "role" ADD COLUMN "removable" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "role" ADD CONSTRAINT "role_code_unique" UNIQUE("code");
