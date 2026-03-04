baca3f4c0a42169fd792c36707ea77c521131e80
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-04 12:36:39 -0300

    [#157] - Address code review findings

--- src/api/container/container.spec.ts
+++ src/api/container/container.spec.ts
@@ -95,6 +95,7 @@ describe('DI Container', () => {
 
 		it('should resolve tokenMaintenanceService to the same instance as authService', () => {
 			expect(container.cradle.tokenMaintenanceService).toBe(container.cradle.authService);
+			expect(container.cradle.tokenMaintenanceService).toBeInstanceOf(AuthService);
 		});
 	});
 
