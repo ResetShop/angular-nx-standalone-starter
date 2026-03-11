102dc68000e5dc102edc293f676ed223827ef153
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-11 15:11:17 -0300

    [#122] - Remove shell page components

--- src/app/pages/dashboard/dashboard.routes.ts
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -12,11 +12,6 @@ export default [
 				title: 'Salud',
 				loadComponent: () => import('./pages/health/health'),
 			},
-			{
-				path: 'users',
-				title: 'Usuarios',
-				loadComponent: () => import('./users/users-list/users-list'),
-			},
 			// TODO: Remove access to this route once the project setup is completed
 			{
 				path: 'welcome',
