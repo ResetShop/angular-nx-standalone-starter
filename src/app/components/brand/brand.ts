a7de6ba7160d174c5e3a351ebf817c05bcfa4e78
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-16 15:19:41 -0300

    [#208] - Add explicit spinner sizing and data-icon attributes to existing usages

--- src/app/components/brand/brand.ts
+++ src/app/components/brand/brand.ts
@@ -10,7 +10,7 @@ import { featherRefreshCw } from '@ng-icons/feather-icons'
 	template: `
 		<!--	TODO: Replace with the navigation to your home page / initial page -->
 		<a [routerLink]="['welcome']" [fullWidth]="true" appButton variant="default" size="sm" class="gap-2 font-semibold">
-			<ng-icon name="featherRefreshCw" />
+			<ng-icon name="featherRefreshCw" data-icon="start" />
 			<span>Reset Starter Repo</span>
 		</a>
 	`,
