51a938d10889baa96aa259c6bd0e3346c9af58fb
Ramiro Olivencia <ramiro@olivencia.com.ar>
2026-03-19 20:05:47 -0300

    [#211] - Add frontend permission constants and @constants path alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
 			'@contracts': '/src/contracts',
 			'@components': '/src/app/components',
 			'@configs': '/src/app/configs',
+			'@constants/permissions': '/src/app/constants/permissions',
 			'@domain': '/src/app/domain',
 			'@guards': '/src/app/guards',
 			'@interfaces': '/src/app/interfaces',
